502bfd89e2983ef48f7d357b56c5da957691ea58
BT_2S14_03 <@>
2025-01-28 10:02:53 -0500

    parsing file input

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         File f = new File("src/InputFile");
+        Scanner s=  null;
         try {
-            Scanner s = new Scanner(f);
+            s = new Scanner(f);
         }
         catch (FileNotFoundException _){
             System.exit(1);
         }
+        String fileData = "";
+        while (s.hasNextLine())
+        {
+            fileData += s.nextLine() + "\n";
+        }
+        String [] fileArray = fileData.split("\n");
+        for (int i = 0; i < fileArray.length; i++)
+        {
+            String[] splits = fileArray[i].split(",");
+            String chop = splits[splits.length - 1];
+            String getHi [] = chop.split("\\|");
+            splits[splits.length - 1] = getHi[0];
+            System.out.println(Arrays.toString(splits));
+        }
+
 
 
 
